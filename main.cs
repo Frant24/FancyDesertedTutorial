8cbe112bb1e6232ebf8d21801d371324bf5efcc0
Frant24 <36748891+Frant24@users.noreply.github.com>
2023-10-06 06:20:49 +0000

    Main upload

--- main.cs
+++ main.cs
@@ -1,7 +1,32 @@
 using System;
 
-class Program {
-  public static void Main (string[] args) {
-    Console.WriteLine ("Hello World");
-  }
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Enter a number to check if it's a perfect number:");
+        int number = int.Parse(Console.ReadLine());
+
+        if (IsPerfect(number))
+        {
+            Console.WriteLine("Perfect Number");
+        }
+        else
+        {
+            Console.WriteLine("Not A Perfect Number");
+        }
+    }
+
+    static bool IsPerfect(int n)
+    {
+        int sum = 0;
+        for (int i = 1; i <= n / 2; i++)
+        {
+            if (n % i == 0)
+            {
+                sum += i;
+            }
+        }
+        return sum == n;
+    }
 }
